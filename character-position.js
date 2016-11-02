c8db3b34af4de01d4f296fd93f979cb956cab182
Ryo <kanadajinlee@yahoo.ca>
2016-11-02 19:10:50 +0000

    Finished char position activity

---
+++ character-position.js
@@ -0,0 +1,16 @@
+var positionLetters = function(str) {
+  var characters = str.split(' ').join('').split('');
+  var positionOfChars = {};
+
+  for (var i = 0; i < characters.length; i++) {
+    if (!positionOfChars.hasOwnProperty(characters[i])) {
+      positionOfChars[characters[i]] = [i];
+    } else {
+      positionOfChars[characters[i]].push(i);
+    }
+  }
+  console.log(positionOfChars);
+}
+
+var userInput = process.argv.slice(2);
+positionLetters(userInput[0]);
